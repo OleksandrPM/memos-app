2006463ba885633350812b3d6d20cb50f017194a
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-06-26 18:05:50 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Memos tech task" />
+</template>
+
+<style scoped></style>
