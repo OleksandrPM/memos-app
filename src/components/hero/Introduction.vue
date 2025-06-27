70a098407515cd7f4788e10ac281aa2b50f422d6
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-06-27 19:29:58 +0200

    Add content to Hero section

---
+++ src/components/hero/Introduction.vue
@@ -0,0 +1,18 @@
+<template>
+  <h4>{{ name }}</h4>
+  <h1>{{ motto }}</h1>
+  <p>
+    {{ text }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: "Introduction",
+  props: {
+    name: { type: String, default: "Introduction`s name" },
+    motto: { type: String, default: "Motto" },
+    text: { type: String, default: "Introduction`s text" },
+  },
+};
+</script>
--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -1,10 +1,21 @@
 <script setup>
 import Button from "../Button.vue";
+import Introduction from "../hero/Introduction.vue";
+import projects from "../../content/projects.json";
 </script>
 
 <template>
   <section>
-    <h1>Hero section</h1>
+    <h2>Hero section</h2>
+    <ul>
+      <li v-for="(project, index) in projects" :key="index">
+        <Introduction
+          :name="project.name"
+          :motto="project.motto"
+          :text="project.text"
+        />
+      </li>
+    </ul>
     <Button label="Create an account" @click="handleClick" />
   </section>
 </template>
