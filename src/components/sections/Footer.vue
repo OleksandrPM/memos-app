3c4d914a0e76c41a8c70ee627a03c66eda6e32c9
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-07-01 00:42:13 +0200

    Add footer content

--- src/components/sections/Footer.vue
+++ src/components/sections/Footer.vue
@@ -1,5 +1,94 @@
 <template>
-  <footer><div class="container">Footer</div></footer>
+  <footer>
+    <div class="container">
+      <h2 class="visually-hidden">Footer</h2>
+      <div
+        class="py-3 d-flex align-items-center justify-content-between border-bottom"
+      >
+        <h3>Startup 3</h3>
+        <ul class="list-unstyled d-flex gap-2">
+          <li>
+            <a
+              class="text-reset text-decoration-none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Privacy Policy</a
+            >
+          </li>
+          <li>
+            <a
+              class="text-reset text-decoration-none"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Terms</a
+            >
+          </li>
+          <li>
+            <a
+              class="text-reset text-decoration-none"
+              href="https://x.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg class="" width="39" height="32">
+                <use href="/sprite.svg#twitter"></use>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              class="text-reset text-decoration-none"
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg width="18" height="32">
+                <use href="/sprite.svg#facebook"></use>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              class="text-reset text-decoration-none"
+              href="https://www.google.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg width="48" height="32">
+                <use href="/sprite.svg#google-plus"></use>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="d-flex align-items-center justify-content-between py-3">
+        <nav>
+          <ul class="list-unstyled d-flex gap-2">
+            <li>
+              <a class="text-reset text-decoration-none" href="#">Features</a>
+            </li>
+            <li>
+              <a class="text-reset text-decoration-none" href="#"
+                >Pricing Plans</a
+              >
+            </li>
+            <li>
+              <a class="text-reset text-decoration-none" href="#">Our Works</a>
+            </li>
+            <li>
+              <a class="text-reset text-decoration-none" href="#">Brands</a>
+            </li>
+            <li>
+              <a class="text-reset text-decoration-none" href="#">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+        <p>© 2025. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
